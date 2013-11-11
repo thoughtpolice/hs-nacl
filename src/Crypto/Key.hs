7338f3c4df7afd7c7b992e3da896e18e137db455
Austin Seipp <aseipp@pobox.com>
2013-11-10 19:17:29 -0600

    Sensible Read/Show instances for keys/nonces
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- src/Crypto/Key.hs
+++ src/Crypto/Key.hs
@@ -13,13 +13,48 @@ module Crypto.Key
        ( SecretKey(..) -- :: *
        , PublicKey(..) -- :: *
        ) where
-import           Data.ByteString
+import           Data.ByteString (ByteString)
+import qualified Data.ByteString as B
+import           Data.Word
+import           Numeric         (readHex)
+import           Text.Printf     (printf)
 
 -- | A @'SecretKey'@ created by @'createKeypair'@. Be sure to keep
 -- this safe!
 newtype SecretKey t = SecretKey { unSecretKey :: ByteString }
-        deriving (Eq, Show, Ord)
+        deriving (Eq, Ord)
+
+instance Show (SecretKey t) where
+  show (SecretKey xs) = writeBytes xs
+
+instance Read (SecretKey t) where
+  readsPrec _ xs = [(SecretKey $ readBytes xs, "")]
 
 -- | A @'PublicKey'@ created by @'createKeypair'@.
 newtype PublicKey t = PublicKey { unPublicKey :: ByteString }
-        deriving (Eq, Show, Ord)
+        deriving (Eq, Ord)
+
+instance Show (PublicKey t) where
+  show (PublicKey xs) = writeBytes xs
+
+instance Read (PublicKey t) where
+  readsPrec _ xs = [(PublicKey $ readBytes xs, "")]
+
+--
+-- Utilities
+--
+
+writeBytes :: ByteString -> String
+writeBytes = concatMap (printf "%02x") . B.unpack
+
+readBytes :: String -> ByteString
+readBytes = B.pack . map reader . chunk 2
+  where reader :: String -> Word8
+        reader x = case readHex x of
+          [(a, "")] -> a
+          _         -> error "invalid key bytes in readBytes"
+
+        chunk :: Int -> [a] -> [[a]]
+        chunk _ [] = []
+        chunk n xs = y1 : chunk n y2
+          where (y1, y2) = splitAt n xs
--- src/Crypto/Nonce.hs
+++ src/Crypto/Nonce.hs
@@ -26,15 +26,23 @@ import           Foreign.Ptr
 import           Data.ByteString          as S
 import           Data.ByteString.Internal as SI
 import           Data.ByteString.Unsafe   as SU
-
+import           Numeric
 import           System.Crypto.Random
+import           Text.Printf              (printf)
 
 -- | A @'Nonce' t@ is a @'ByteString'@ parameterized by some type @t@,
 -- which designates what interface the @'Nonce'@ is used for. This
 -- provides more type-safety to ensure you don't confuse them when
 -- performing encryption.
 newtype Nonce t = Nonce { unNonce :: ByteString }
-  deriving (Eq, Show)
+  deriving Eq
+
+instance Show (Nonce t) where
+  show (Nonce xs) = writeBytes xs
+
+instance Read (Nonce t) where
+  readsPrec _ xs = [(Nonce $ readBytes xs, "")]
+
 
 -- | A common interface for different nonces.
 class Nonces t where
@@ -65,6 +73,25 @@ incBS n =
       SI.memcpy out (castPtr b) (fromIntegral blen)
     c_incnonce out (fromIntegral (S.length n))
 {-# INLINE incBS #-}
+
+--
+-- Utilities
+--
+
+writeBytes :: ByteString -> String
+writeBytes = Prelude.concatMap (printf "%02x") . S.unpack
+
+readBytes :: String -> ByteString
+readBytes = S.pack . Prelude.map reader . chunk 2
+  where reader :: String -> Word8
+        reader x = case readHex x of
+          [(a, "")] -> a
+          _         -> error "invalid key bytes in readBytes"
+
+        chunk :: Int -> [a] -> [[a]]
+        chunk _ [] = []
+        chunk n xs = y1 : chunk n y2
+          where (y1, y2) = Prelude.splitAt n xs
 
 --
 -- FFI
