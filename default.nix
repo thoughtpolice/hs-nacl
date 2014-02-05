5c08b8f9ee47690878dd46a977a08c161e13fea2
Austin Seipp <aseipp@pobox.com>
2014-02-04 20:37:56 -0600

    More nix metadata
    Signed-off-by: Austin Seipp <aseipp@pobox.com>

--- default.nix
+++ default.nix
@@ -9,8 +9,16 @@ in cabal.mkDerivation (self: {
   pname = "nacl";
   version = "0.0.0.0";
   src = ./.;
+  isLibrary = true;
   buildDepends = [ base64Bytestring filepath ];
   testDepends  = [ QuickCheck hlint doctest ];
   buildTools   = [ cabalInstall_1_18_0_2 ];
   enableSplitObjs = false;
+
+  meta = {
+    homepage    = "http://github.com/thoughtpolice/hs-nacl";
+    description = "Modern Haskell Cryptography";
+    license     = self.stdenv.lib.licenses.bsd3;
+    platforms   = self.ghc.meta.platforms;
+  };
 })
